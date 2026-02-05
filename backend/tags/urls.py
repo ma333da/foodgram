b554776ce75d0a99ffc4cb45d9eaa5ef77273da6
ma333da <ma333da@gmail.com>
2026-02-05 10:06:07 +0300

    djksfkjssdkjfh

--- backend/tags/urls.py
+++ backend/tags/urls.py
@@ -1,12 +1,10 @@
-from django.db import router
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from .views import TagViewSet
 
 router = DefaultRouter()
-router.register('tags', TagViewSet, basename='tags')
-
+router.register('tags', TagViewSet) 
 
 urlpatterns = [
     path('', include(router.urls)),
