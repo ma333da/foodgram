96a550d8759fe1435c6f03c94de897fb4b7c00cb
ma333da <ma333da@gmail.com>
2026-02-05 10:23:46 +0300

    hhh

--- backend/tags/urls.py
+++ backend/tags/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 from .views import TagViewSet
 
 router = DefaultRouter()
-router.register('tags', TagViewSet) 
+router.register('tags', TagViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
