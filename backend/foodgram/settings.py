99c7310759dd984c81936fe363076c0fa91ce69a
ma333da <ma333da@gmail.com>
2026-02-21 20:16:00 +0300

    18

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -21,7 +21,7 @@ DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 ALLOWED_HOSTS = ['*']
-
+CSRF_TRUSTED_ORIGINS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
