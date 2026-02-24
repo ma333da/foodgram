234e92a0047348cddad87fa772819c894c8d3dd5
ma333da <ma333da@gmail.com>
2026-02-24 11:44:44 +0300

    37

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -86,7 +86,7 @@ class IngredientSerializer(serializers.ModelSerializer):
 
 
 class IngredientAmountSerializer(serializers.ModelSerializer):
-    id = serializers.ReadOnlyField(source='ingredient.id')
+    id = serializers.IntegerField(source='ingredient.id')
     name = serializers.ReadOnlyField(source='ingredient.name')
     measurement_unit = serializers.ReadOnlyField(
         source='ingredient.measurement_unit'
