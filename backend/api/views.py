ec40f5633a3405d4033ca9fa96a682910e80ca11
ma333da <ma333da@gmail.com>
2026-02-25 14:57:41 +0300

    46

--- backend/api/views.py
+++ backend/api/views.py
@@ -65,6 +65,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         user = self.request.user
         queryset = Recipe.objects.all().select_related('author')
+        
         if user.is_authenticated:
             is_favorite_annotation = Exists(
                 Favorite.objects.filter(user=user, recipe=OuterRef('pk'))
@@ -76,8 +77,8 @@ class RecipeViewSet(viewsets.ModelViewSet):
                 is_favorited=is_favorite_annotation,
                 is_in_shopping_cart=is_in_shopping_cart_annotation,
             )
-
-            return queryset
+        
+        return queryset 
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -208,7 +209,7 @@ class FoodgramUserViewSet(UserViewSet):
             return Response(status=status.HTTP_204_NO_CONTENT)
         serializer = FoodgramUserSerializer(
             user,
-            data={'avatar': request.data.get('avatar')},
+            data={'avatar': request.FILES.get('avatar')},
             partial=True,
             context={'request': request},
         )
